b7d2d72cd4034ed2bfc21bca0d2bd24bcdce7cb0
krivonos-alex <krivonos-al@mail.ru>
2018-12-25 01:27:52 +0300

    Homework for lesson 6

---
+++ Homeworks/src/ru/mail/krivonos/al/lesson/six/ConsoleInputAndRandom.java
@@ -0,0 +1,66 @@
+package ru.mail.krivonos.al.lesson.six;
+
+import java.util.Random;
+
+public class ConsoleInputAndRandom {
+    private Random random = new Random();
+
+    void printNumberNumeralsSumAndCounter(int targetNumber) {
+        int numeralsCounter = 0;
+        for (int i = targetNumber; i != 0; i /= 10) {
+            numeralsCounter++;
+        }
+        int decimalCounter = 1;
+        for (int i = 0; i < numeralsCounter - 1; i++) {
+            decimalCounter *= 10;
+        }
+        int numeralsSum = 0;
+        int targetNumeral = 0;
+        for (int i = 0; i < numeralsCounter; i++) {
+            targetNumeral = targetNumber / decimalCounter;
+            if (i == 0) {
+                numeralsSum += targetNumeral;
+            } else {
+                numeralsSum += targetNumeral - ((targetNumeral / 10) * 10);
+            }
+            decimalCounter /= 10;
+        }
+        System.out.print("Your number contains ");
+        System.out.print(numeralsCounter);
+        System.out.print(" numerals and their sum is ");
+        System.out.println(numeralsSum);
+    }
+
+    private void printArray(int[] targetArray) {
+        System.out.println("Your array:");
+        for (int element: targetArray) {
+            System.out.print(element);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+   int[] fillArrayWithRandomNumbers(int arraySize,  int startOfScope, int endOfScope) {
+        int[] targetArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            targetArray[i] = random.nextInt(endOfScope - startOfScope + 1) + startOfScope;
+        }
+        printArray(targetArray);
+        return targetArray;
+    }
+
+    int countEvenInArray(int[] targetArray) {
+        int evenCounter = 0;
+        for (int element: targetArray) {
+            if (element % 2 == 0) {
+                evenCounter++;
+            }
+        }
+        return evenCounter;
+    }
+
+    void printRandomNumberFromScope(int scopeBorder) {
+        int targetNumber = random.nextInt(2 * scopeBorder + 1) - scopeBorder;
+        System.out.println(targetNumber);
+    }
+}
---
+++ Homeworks/src/ru/mail/krivonos/al/lesson/six/Main.java
@@ -0,0 +1,30 @@
+package ru.mail.krivonos.al.lesson.six;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ConsoleInputAndRandom consoleInputAndRandom = new ConsoleInputAndRandom();
+
+        System.out.println("Input your number:");
+        int targetNumber = scanner.nextInt();
+        consoleInputAndRandom.printNumberNumeralsSumAndCounter(targetNumber);
+
+        System.out.println("Input array size:");
+        int arraySize = scanner.nextInt();
+        System.out.println("Input start of scope:");
+        int startOfScope = scanner.nextInt();
+        System.out.println("Input end of scope:");
+        int endOfScope = scanner.nextInt();
+        int[] targetArray = consoleInputAndRandom.fillArrayWithRandomNumbers(arraySize, startOfScope, endOfScope);
+        int evenCounter = consoleInputAndRandom.countEvenInArray(targetArray);
+        System.out.print("Your array contains ");
+        System.out.print(evenCounter);
+        System.out.println(" even numbers");
+
+        System.out.println("Input scope border:");
+        int scopeBorder = scanner.nextInt();
+        consoleInputAndRandom.printRandomNumberFromScope(scopeBorder);
+    }
+}
